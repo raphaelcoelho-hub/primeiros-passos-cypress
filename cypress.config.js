2639e8ce0edf2e8c5280c74c0f8e224d9703d999
RaphaelCoelho <raphaelmnc@gmail.com>
2026-06-26 13:26:23 -0300

    base url added

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
-  e2e: {
+   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
-});
+
+          baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php',
+    },
+  });
