92803791dabdf268e65eb5455fbf11da2f3d389f
RaphaelCoelho <raphaelmnc@gmail.com>
2026-06-29 13:20:00 -0300

    aula 21

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    allowCypressEnv: false,
 
           baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php',
     },
--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -8,16 +8,19 @@ describe('Orange HRM Tests', () => {
    loginButton: "[type='submit']",
    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
    dashboardGrid: ".orangehrm-dashboard-grid",
-   wrongCredentialAlert: "[role='alert']"
+   wrongCredentialAlert: "[role='alert']",
+   myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]'
   }
          
-  it('Login - Succes', () => {
+  it.only('User Info Update - Success', () => {
+
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()  
+    cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
   })
   it('Login - Fail', () => {
     cy.visit('/auth/login')
